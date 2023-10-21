c709962a6abd9ba012b7b55c5c23aa16530b73d0
Explotador72 <83897947+Explotador72@users.noreply.github.com>
2023-10-21 14:27:04 +0200

    Delete Fooocus_api directory

--- Fooocus_api/README.md
+++
@@ -1,2 +0,0 @@
-# Fooocus_api
- 
--- Fooocus_api/repository_version.py
+++
@@ -1,7 +0,0 @@
-import os
-
-fooocus_version = '2.0.93'
-comfy_commit_hash = os.environ.get(
-    'COMFY_COMMIT_HASH', "9bfec2bdbf0b0d778087a9b32f79e57e2d15b913")
-fooocus_commit_hash = os.environ.get(
-    'FOOOCUS_COMMIT_HASH', "b42e96a52d92265fb966954090aa43340f199008")
