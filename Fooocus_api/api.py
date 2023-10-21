3d4ef4c88b5c5e8ea9f1436dbca0a2372da81298
Explotador72 <83897947+Explotador72@users.noreply.github.com>
2023-10-21 10:52:51 +0200

    Update api.py

--- Fooocus_api/api.py
+++ Fooocus_api/api.py
@@ -1,10 +1,11 @@
-from typing import List
-from fastapi import Depends, FastAPI, File, Header, Query, Response, UploadFile
+from typing import List, Optional
+from fastapi import Depends, FastAPI, Header, Query, UploadFile
+from fastapi.params import File
 import uvicorn
-from Fooocus_api.api_utils import generation_output, narray_to_base64img, narray_to_bytesimg
-from Fooocus_api.models import GeneratedImageBase64, GenerationFinishReason, ImgInpaintOrOutpaintRequest, ImgUpscaleOrVaryRequest, Text2ImgRequest
-from Fooocus_api.task_queue import TaskQueue
-from Fooocus_api.worker import process_generate
+from fooocusapi.api_utils import generation_output, req_to_params
+from fooocusapi.models import GeneratedImageBase64, ImgInpaintOrOutpaintRequest, ImgPromptRequest, ImgUpscaleOrVaryRequest, Text2ImgRequest
+from fooocusapi.task_queue import TaskQueue
+from fooocusapi.worker import process_generate
 
 app = FastAPI()
 
@@ -28,6 +29,10 @@ img_generate_responses = {
     }
 }
 
+@app.get("/")
+def home():
+    return "Swagger-UI to: /docs"
+
 
 @app.post("/v1/generation/text-to-image", response_model=List[GeneratedImageBase64], responses=img_generate_responses)
 def text2img_generation(req: Text2ImgRequest, accept: str = Header(None),
@@ -42,7 +47,7 @@ def text2img_generation(req: Text2ImgRequest, accept: str = Header(None),
     else:
         streaming_output = False
 
-    results = process_generate(req)
+    results = process_generate(req_to_params(req))
     return generation_output(results, streaming_output)
 
 
@@ -60,7 +65,7 @@ def img_upscale_or_vary(input_image: UploadFile, req: ImgUpscaleOrVaryRequest = 
     else:
         streaming_output = False
 
-    results = process_generate(req)
+    results = process_generate(req_to_params(req))
     return generation_output(results, streaming_output)
 
 
@@ -78,10 +83,29 @@ def img_inpaint_or_outpaint(input_image: UploadFile, req: ImgInpaintOrOutpaintRe
     else:
         streaming_output = False
 
-    results = process_generate(req)
+    results = process_generate(req_to_params(req))
+    return generation_output(results, streaming_output)
+
+
+@app.post("/v1/generation/image-prompt", response_model=List[GeneratedImageBase64], responses=img_generate_responses)
+def img_prompt(cn_img1: Optional[UploadFile] = File(None),
+               req: ImgPromptRequest = Depends(ImgPromptRequest.as_form),
+               accept: str = Header(None),
+               accept_query: str | None = Query(None, alias='accept', description="Parameter to overvide 'Accept' header, 'image/png' for output bytes")):
+    if accept_query is not None and len(accept_query) > 0:
+        accept = accept_query
+
+    if accept == 'image/png':
+        streaming_output = True
+        # image_number auto set to 1 in streaming mode
+        req.image_number = 1
+    else:
+        streaming_output = False
+
+    results = process_generate(req_to_params(req))
     return generation_output(results, streaming_output)
 
 
 def start_app(args):
-    uvicorn.run("Fooocus_api.api:app", host=args.host,
+    uvicorn.run("fooocusapi.api:app", host=args.host,
                 port=args.port, log_level=args.log_level)
